380c2aedaeb40b067cd0ae980985c732d48bb325
Tomasz Szyndler <tsszyndler@gmail.com>
2026-02-23 15:48:16 +0100

    step 3: double jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,23 +7,41 @@ public class Player : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
-
+    
     private Rigidbody2D rb;
     private bool isGrounded;
     private Animator animator;
+
+    public int extraJumpsValue = 1;
+    private int extraJumps;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        extraJumps = extraJumpsValue;
     }
     void Update()
     {
         float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (isGrounded)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            extraJumps = extraJumpsValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            }
+            else if(extraJumps > 0)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                extraJumps--;
+            }
         }
 
         SetAnimation(moveInput);
