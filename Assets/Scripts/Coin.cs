feeda356f30ef4255963643f118aa4796241d443
Tomasz Szyndler <tsszyndler@gmail.com>
2026-02-24 15:19:34 +0100

    step 14: coin UI, more coins added

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
+using TMPro;
 
 public class Coin : MonoBehaviour
 {
     public AudioClip coinClip;
+    public int coinsToGive = 1;
+    private TextMeshProUGUI coinText;
+
+    private void Start()
+    {
+        coinText = GameObject.FindWithTag("CoinText").GetComponent<TextMeshProUGUI>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             Player player = collision.gameObject.GetComponent<Player>();
-            player.coins += 1;
+            player.coins += coinsToGive;
             player.PlaySFX(coinClip, 0.4f);
+            coinText.text = player.coins.ToString();
             Destroy(gameObject);
         }
     }
