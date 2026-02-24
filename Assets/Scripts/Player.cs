3a7326cd10c70a9cd5159af2d8a7113ef8c80f19
Tomasz Szyndler <tsszyndler@gmail.com>
2026-02-24 10:51:13 +0100

    step 7: cinemachine & health bar

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class Player : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
+    public Image healthImage;
 
     private Rigidbody2D rb;
     private bool isGrounded;
@@ -51,6 +53,8 @@ public class Player : MonoBehaviour
         }
 
         SetAnimation(moveInput);
+
+        healthImage.fillAmount = health / 100f;
     }
 
     private void FixedUpdate()
