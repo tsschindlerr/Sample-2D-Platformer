3f6dca6114c483845ea8eb91bbd8ffb551bc4824
Tomasz Szyndler <tsszyndler@gmail.com>
2026-02-25 08:23:12 +0100

    step 17: level select screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,9 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartGame()
+    public GameObject startMainMenu;
+    public GameObject levelSelect;
+    public void StartGame(string sceneName)
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void GoToLevelSelect()
+    {
+        startMainMenu.SetActive(false);
+        levelSelect.SetActive(true);
     }
 
     public void QuitGame()
